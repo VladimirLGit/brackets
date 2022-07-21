44e5071a12afad9458ee0d38496e5b6e9473ebba
VladimirLGit <1077control@gmail.com>
2022-07-21 12:09:34 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,26 +1,69 @@
+
 module.exports = 
 function check(str, bracketsConfig) {
-  let strCheck = String(str).split('');
-  
-  Array.from(bracketsConfig).reverse().forEach(element => {
-    let intBrackets =  0;  
-    let fromIndexBegin = 0;
-    let fromIndexEnd = 0;
-    while ((fromIndexBegin != -1) && (fromIndexEnd != -1))  {
-      fromIndexBegin = strCheck.lastIndexOf(element[0]);
-      fromIndexEnd = strCheck.indexOf(element[1], fromIndexBegin);
-      if ((fromIndexBegin != -1) && (fromIndexEnd != -1) /*&& (fromIndexEnd - fromIndexBegin === 1)*/) {
-        strCheck.splice(fromIndexBegin,1);
-        strCheck.splice(fromIndexEnd-1,1);
-      }
-      else {
-        break;
-      }
-    }    
+  let bracketOC = {};
+  let bracketOne = {};
+  bracketsConfig.forEach(element => {
+    if (element[0] != element[1]) {
+      bracketOC[element[0]] = element[1];
+    }
+    else  
+      bracketOne[element[0]] = element[1];
   });
-  return strCheck.length === 0;
+  let stackBrackets = [];
+
+  //let brck = String(str).matchAll(/(\()|(\))/g)
+
+  for (let index = 0; index < str.length; index++) {
+    const element = str[index];    
+    if (bracketOne[element]) {
+      //значит закрывающая скобка
+      if (stackBrackets.length > 0) {
+        if (bracketOne[stackBrackets[stackBrackets.length-1]] === element) {
+          stackBrackets.pop(); //удалить открывающую скобку            
+        }
+        else { 
+          //добавить открывающую скобку            
+          stackBrackets.push(element);
+        }
+      }
+      else
+        stackBrackets.push(element);
+    } 
+    else       
+    if (!bracketOC[element]) {
+      //значит закрывающая скобка
+      if (stackBrackets.length > 0) {
+        if (bracketOC[stackBrackets[stackBrackets.length-1]] === element) {
+          stackBrackets.pop(); //удалить открывающую скобку
+        }
+        else
+          break;
+      }
+      else  
+        stackBrackets.push(element);        
+    }
+    else {
+      stackBrackets.push(element);
+    }
+        
+  }
+
+  return stackBrackets.length === 0;
 }
-// const config1 = [['(', ')']];
-// const config2 = [['(', ')'], ['[', ']']];
-// console.log(check('((()))()', config1))
-// console.log(check('[(])', config2))
+
+
+//const config1 = [['(', ')']];
+//const config2 = [['(', ')'], ['[', ']']];
+//const config4 = [['|', '|']];
+//const config5 = [['(', ')'], ['|', '|']];
+//const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+//const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+//console.log(check('((()))()', config1))
+//console.log(check('[(])', config2))
+//console.log(check('|()|(||)||', config5));
+//console.log(check('[]()(', config2));
+//console.log(check('||', config4));
+//console.log(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()', config7));
+//console.log(check('8888877878887777777888888887777777887887788788887887777777788888888887788888', config6));
+
